14f0108395a6bee86f7746c470f3be0ebe7b7660
Headon1729 <f20170630@pilani.bits-pilani.ac.in>
2021-02-13 12:27:00 +0530

    added title

--- secondary_study/neo_4_j.py
+++ secondary_study/neo_4_j.py
@@ -4,7 +4,8 @@ from paper import Paper
 
 class Graph(py2neo.Graph):
 
-  author_names = set()
+  author_set = set()
+  year_set = set()
 
   def __init__(self, *args, **kwargs):
     '''Initializes graph from data from config (if arguments not specified)'''
@@ -21,22 +22,37 @@ class Graph(py2neo.Graph):
     # print(paper.author)
     for author in paper.author.split(' and '):
       author = author.strip()
+      year = paper.year
       author_node = self.add_author(author)
+      year_node = self.add_year(year)
       rel = py2neo.Relationship(author_node, 'PUBLISHED', paper_node)
+      rel2 = py2neo.Relationship(author_node, 'PUBLISHED_IN', year_node)
       self.create(rel)
+      self.create(rel2)
+
+
 
   def add_author(self, author):
-    if author not in self.author_names:
+    if author not in self.author_set:
         author_node = py2neo.Node('Author', name=author)
-        self.author_names.add(author)
+        self.author_set.add(author)
         self.create(author_node)
     else:
       author_node = self.nodes.match('Author', name=author).first()
     return author_node
 
+  def add_year(self, year):
+    if year not in self.year_set:
+      year_node = py2neo.Node('Year', name=year)
+      self.year_set.add(year)
+      self.create(year_node)
+    else:
+      year_node = self.nodes.match('Year', name=year).first()
+    return year_node
+
   def add_papers(self, papers):
     assert isinstance(papers, list), 'papers is not a list'
-    self.author_names = set()
+    self.author_set = set()
     for paper in papers:
       self.add_paper(paper)
 
--- secondary_study/utils.py
+++ secondary_study/utils.py
@@ -5,7 +5,7 @@ from neo_4_j import Graph
 
 
 if __name__ == '__main__':
-  filepath = '/home/laozi/Manas/LIFE/MONEY/Acads/lop_/secondary_study/papers copy.xlsx'
+  filepath = './papers copy.xlsx'
   papers = Paper.papers_from_excel(filepath)
   graph = Graph()
   graph.delete_all()
